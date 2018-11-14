5b520a618cdd5de5cd16793ca0f9b379a9ea1279
Matias Ignacio Almad <matias_almad@hotmail.com>
2018-11-14 23:41:15 +0000

    se subio zip del serial y se modifico el codigo del auto.

--- robot_car/robot_car.ino
+++ robot_car/robot_car.ino
@@ -1,192 +1,206 @@
-#include <NewPing.h>
-#include <Servo.h>
+/*#include <NewPing.h>
+  #include <Servo.h>*/
 #include <LiquidCrystal.h>
-
-#define TRIG_PIN A4
-#define ECHO_PIN A5
-#define MAX_DISTANCE 200
-
-// Entradas de modo
-#define NORMAL_MODE_INPUT 1
-#define TEST_MODE_INPUT 2
-#define MAINTENANCE_MODE_INPUT 3
-
+#include <SoftwareSerial.h>
+/*
+  #define TRIG_PIN A4
+  #define ECHO_PIN A5
+  #define MAX_DISTANCE 200
+*/
 // Definición de modos
-#define NORMAL_MODE 1
-#define TEST_MODE 2
-#define MAINTENANCE_MODE 3
-#define UNKNOWN_MODE 3
+#define NORMAL_MODE 0
+#define TEST_MODE 1
+#define MAINTENANCE_MODE 2
+
+#define ADELANTE 97
+#define ATRAS 101
+#define IZQUIERDA 98
+#define DERECHA 100
+#define ENCENDIDO 102
+#define APAGADO 103
+#define OPCION 99
+
+SoftwareSerial BT(10, 11);
 
 // initialize the library by associating any needed LCD interface pin
 // with the arduino pin number it is connected to
-const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
+
+int current_mode = -1;
+String modos[] = {"NORMAL","TEST","MANTENIMIENTO"};
+const int rs = 7, en = 6, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+/*
+  NewPing sonar(TRIG_PIN, ECHO_PIN, MAX_DISTANCE);
+  Servo myservo;
 
-NewPing sonar(TRIG_PIN, ECHO_PIN, MAX_DISTANCE);
-Servo myservo;
-
-boolean goesForward = false;
-int distance = 100;
-int speedSet = 0;
-boolean alerted_unknown_mode = false;
-
-const int motorPin1  = 11;
-const int motorPin2  = 10;
-//Motor B
-const int motorPin3  = 6;
-const int motorPin4  = 5;
-
+  boolean goesForward = false;
+  int distance = 100;
+  int speedSet = 0;*/
+/*
+  const int motorPin1  = 11;
+  const int motorPin2  = 10;
+  //Motor B
+  const int motorPin3  = 6;
+  const int motorPin4  = 5;
+*/
 // Retorna el modo de operacion actual.
-byte currentMode() {
-  if (digitalRead(NORMAL_MODE_INPUT) == HIGH) {
-    return NORMAL_MODE;
-  } else if (digitalRead(TEST_MODE_INPUT) == HIGH) {
-    return TEST_MODE;
-  } else if (digitalRead(MAINTENANCE_MODE_INPUT) == HIGH) {
-    return MAINTENANCE_MODE;
-  } else {
-    return UNKNOWN_MODE;
+
+byte change_mode() {
+  current_mode = ++current_mode % 3;
+  return current_mode;
+}
+
+int leer_bluetooth() {
+  if (BT.available())   // Si llega un dato por el puerto BT se envÃ­a al monitor serial
+  {
+    return BT.read();
+  }
+  else
+  {
+    return -1;
   }
 }
 
-int lookRight() {
-  myservo.write(50);
-  delay(500);
-  int distance = readPing();
-  delay(100);
-  myservo.write(115);
-  return distance;
+void escribir_pantalla(String mensaje) {
+  lcd.clear();
+  lcd.print(mensaje);
 }
 
-int lookLeft() {
-  myservo.write(170);
-  delay(500);
-  int distance = readPing();
-  delay(100);
-  myservo.write(115);
-  return distance;
-  delay(100);
-}
-
-int readPing() {
-  delay(70);
-  int cm = sonar.ping_cm();
-  if (cm == 0) {
-    cm = 250;
+void mover_adelante()
+{
+  switch (current_mode)
+  {
+    case NORMAL_MODE:
+      break;
+    case TEST_MODE:
+      escribir_pantalla("ADELANTE");
+      break;
+    case MAINTENANCE_MODE:
+      break;
   }
-  return cm;
 }
 
-void moveStop() {
-  if (currentMode() == MAINTENANCE_MODE) {
-    lcd.print("STOP");
-    return;
+void mover_atras()
+{
+  switch (current_mode)
+  {
+    case NORMAL_MODE:
+      break;
+    case TEST_MODE:
+      escribir_pantalla("ATRAS");
+      break;
+    case MAINTENANCE_MODE:
+      break;
   }
-  analogWrite(motorPin1, 0);
-  analogWrite(motorPin2, 0);
-  analogWrite(motorPin3, 0);
-  analogWrite(motorPin4, 0);
 }
 
-void moveForward() {
-  if (currentMode() == MAINTENANCE_MODE) {
-    lcd.print("FORWARD");
-    return;
+void mover_izquierda()
+{
+  switch (current_mode)
+  {
+    case NORMAL_MODE:
+      break;
+    case TEST_MODE:
+      escribir_pantalla("IZQUIERDA");
+      break;
+    case MAINTENANCE_MODE:
+      break;
   }
-  analogWrite(motorPin1, 180);
-  analogWrite(motorPin2, 0);
-  analogWrite(motorPin3, 180);
-  analogWrite(motorPin4, 0);
-
 }
 
-void moveBackward() {
-  if (currentMode() == MAINTENANCE_MODE) {
-    lcd.print("BACKWARD");
-    return;
+void mover_derecha()
+{
+  switch (current_mode)
+  {
+    case NORMAL_MODE:
+      break;
+    case TEST_MODE:
+      escribir_pantalla("DERECHA");
+      break;
+    case MAINTENANCE_MODE:
+      break;
   }
-  analogWrite(motorPin1, 0);
-  analogWrite(motorPin2, 180);
-  analogWrite(motorPin3, 0);
-  analogWrite(motorPin4, 180);
-
 }
 
-void turnRight() {
-  if (currentMode() == MAINTENANCE_MODE) {
-    lcd.print("TURN RIGHT");
-    return;
+void encendido()
+{
+  switch (current_mode)
+  {
+    case NORMAL_MODE:
+      break;
+    case TEST_MODE:
+      escribir_pantalla("ENCENDIDO");
+      break;
+    case MAINTENANCE_MODE:
+      break;
   }
-  analogWrite(motorPin1, 180);
-  analogWrite(motorPin2, 0);
-  analogWrite(motorPin3, 0);
-  analogWrite(motorPin4, 180);
-  delay(300);
-  moveForward();
-
 }
 
-void turnLeft() {
-  if (currentMode() == MAINTENANCE_MODE) {
-    lcd.print("TURN LEFT");
-    return;
+void apagado()
+{
+  switch (current_mode)
+  {
+    case NORMAL_MODE:
+      break;
+    case TEST_MODE:
+      escribir_pantalla("APAGADO");
+      break;
+    case MAINTENANCE_MODE:
+      break;
   }
-  analogWrite(motorPin1, 0);
-  analogWrite(motorPin2, 180);
-  analogWrite(motorPin3, 180);
-  analogWrite(motorPin4, 0);
-  delay(300);
-  moveForward();
+}
+
+void opcion()
+{
+  change_mode();
+  String mensaje = "MODO " + modos[current_mode];
+  escribir_pantalla(mensaje);
 }
 
 void setup() {
+  BT.begin(9600);
   lcd.begin(16, 2);
-  myservo.attach(9);
-  myservo.write(115);
-  delay(2000);
-  distance = readPing();
-  delay(100);
-  distance = readPing();
-  delay(100);
-  distance = readPing();
-  delay(100);
-  distance = readPing();
-  delay(100);
+  /*
+    myservo.attach(9);
+    myservo.write(115);
+    delay(2000);
+    distance = readPing();
+    delay(100);
+    distance = readPing();
+    delay(100);
+    distance = readPing();
+    delay(100);
+    distance = readPing();
+    delay(100);*/
 }
 
 void loop() {
-  if (currentMode() != UNKNOWN_MODE) {
-    alerted_unknown_mode = false;
-    int distanceR = 0;
-    int distanceL =  0;
-    delay(40);
-    if (distance <= 20) {
-      moveStop();
-      delay(100);
-      moveBackward();
-      delay(300);
-      moveStop();
-      delay(200);
-      distanceR = lookRight();
-      delay(200);
-      distanceL = lookLeft();
-      delay(200);
-
-      if (distanceR >= distanceL) {
-        turnRight();
-        moveStop();
-      } else {
-        turnLeft();
-        moveStop();
-      }
-    } else {
-      moveForward();
-    }
-    distance = readPing();
-  } else {
-    if (!alerted_unknown_mode) {
-      lcd.print("UNKNOWN MODE");
-      alerted_unknown_mode = true;
+  int accion = leer_bluetooth();
+  if (accion > 0)
+  {
+    switch (accion)
+    {
+      case ADELANTE:
+        mover_adelante();
+        break;
+      case ATRAS:
+        mover_atras();
+        break;
+      case IZQUIERDA:
+        mover_izquierda();
+        break;
+      case DERECHA:
+        mover_derecha();
+        break;
+      case ENCENDIDO:
+        encendido();
+        break;
+      case APAGADO:
+        apagado();
+        break;
+      case OPCION:
+        opcion();
+        break;
     }
   }
 }
